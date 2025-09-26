88763354d9de39220ca6da7747db59da867a5f21
user <user@A202_05>
2025-09-26 18:20:26 +0300

    Добавьте файлы проекта.

---
+++ БляКакойтоневсратый весокосный год/БляКакойтоневсратый весокосный год.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+int main() {
+
+	SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int year;
+	cout << "Введите год: ";
+    cin >> year;
+
+    if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+        cout << "Високосный";
+    }
+    else {
+        cout << "Не високосный";
+    }
+
+    return 0;
+}
